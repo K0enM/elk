5a58a5f669f02c2bb00f7cb6e9631b78e991440b
Koen Molenaar <github@sl.koenmolenaar.nl>
2025-03-10 17:03:22 +0100

    Article 3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use mmap::{MapOption, MemoryMap};
 use region::{protect, Protection};
 use std::error::Error;
 use std::io::Write;
@@ -16,29 +17,70 @@ fn main() -> Result<(), Box<dyn Error>> {
     };
     println!("{:#?}", file);
 
-    println!("Disassembling {:?}", input_path);
-    let code_ph = file
+    // println!("Disassembling {:?}", input_path);
+    // let code_ph = file
+    //     .program_headers
+    //     .iter()
+    //     .find(|ph| ph.mem_range().contains(&file.entry_point))
+    //     .expect("segment with entry point not found.");
+
+    // ndisasm(&code_ph.data[..], file.entry_point)?;
+
+    let base = 0x400000_usize;
+
+    println!("Mapping {:?} in memory...", input_path);
+
+    let mut mappings = Vec::new();
+
+    for ph in file
         .program_headers
         .iter()
-        .find(|ph| ph.mem_range().contains(&file.entry_point))
-        .expect("segment with entry point not found.");
+        .filter(|ph| ph.r#type == delf::SegmentType::Load)
+        .filter(|ph| ph.mem_range().end > ph.mem_range().start)
+    {
+        println!("Mapping segment @ {:?} with {:?}", ph.mem_range(), ph.flags);
 
-    ndisasm(&code_ph.data[..], file.entry_point)?;
+        let mem_range = ph.mem_range();
+        let len: usize = (mem_range.end - mem_range.start).into();
+        let start: usize = mem_range.start.0 as usize + base;
+        let aligned_start: usize = align_lo(start);
 
-    println!("Executing {:?} in memory...", input_path);
-    let code = &code_ph.data;
-    unsafe {
-        protect(code.as_ptr(), code.len(), Protection::READ_WRITE_EXECUTE)?;
+        let padding = start - aligned_start;
+        let len = len + padding;
+
+        let addr: *mut u8 = aligned_start as _;
+
+        println!("Addr: {:p}, Padding: {:08x}", addr, padding);
+
+        let map = MemoryMap::new(len, &[MapOption::MapWritable, MapOption::MapAddr(addr)])?;
+
+        println!("Copying segment data...");
+        {
+            let dst = unsafe { std::slice::from_raw_parts_mut(addr.add(padding), ph.data.len()) };
+
+            dst.copy_from_slice(&ph.data[..]);
+        }
+
+        println!("Adjusting permissions...");
+        let mut protection = Protection::NONE;
+        for flag in ph.flags.iter() {
+            protection |= match flag {
+                delf::SegmentFlag::Execute => Protection::READ,
+                delf::SegmentFlag::Write => Protection::WRITE,
+                delf::SegmentFlag::Read => Protection::EXECUTE,
+            }
+        }
+        unsafe {
+            protect(addr, len, protection)?;
+        }
+        mappings.push(map);
     }
 
-    let entry_offset = file.entry_point - code_ph.vaddr;
-    let entry_point = unsafe { code.as_ptr().add(entry_offset.into()) };
+    println!("Jumping to entry point @ {:?}...", file.entry_point);
+    pause("jmp")?;
 
-    println!("        code @ {:?}", code.as_ptr());
-    println!("entry offset @ {:?}", entry_offset);
-    println!("entry point  @ {:?}", entry_point);
     unsafe {
-        jmp(entry_point);
+        jmp((file.entry_point.0 as usize + base) as _);
     }
     Ok(())
 }
@@ -63,4 +105,17 @@ fn ndisasm(code: &[u8], origin: delf::Addr) -> Result<(), Box<dyn Error>> {
 unsafe fn jmp(addr: *const u8) {
     let fn_ptr: fn() = std::mem::transmute(addr);
     fn_ptr();
+}
+
+fn pause(reason: &str) -> Result<(), Box<dyn Error>> {
+    println!("Press Enter to {}...", reason);
+    {
+        let mut s = String::new();
+        std::io::stdin().read_line(&mut s)?;
+    }
+    Ok(())
+}
+
+fn align_lo(x: usize) -> usize {
+    x & !0xFFF
 }
