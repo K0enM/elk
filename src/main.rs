5ee1d601076195178fce70f083f2f317096a1dae
Koen Molenaar <github@sl.koenmolenaar.nl>
2025-03-12 14:31:14 +0100

    Article 4

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use mmap::{MapOption, MemoryMap};
 use region::{protect, Protection};
 use std::error::Error;
-use std::io::Write;
-use std::process::{Command, Stdio};
+use std::mem::transmute;
+use std::ptr::copy_nonoverlapping;
 use std::{env, fs};
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -17,51 +17,69 @@ fn main() -> Result<(), Box<dyn Error>> {
     };
     println!("{:#?}", file);
 
-    // println!("Disassembling {:?}", input_path);
-    // let code_ph = file
-    //     .program_headers
-    //     .iter()
-    //     .find(|ph| ph.mem_range().contains(&file.entry_point))
-    //     .expect("segment with entry point not found.");
-
-    // ndisasm(&code_ph.data[..], file.entry_point)?;
-
+    let rela_entries = file.read_rela_entries().unwrap_or_else(|e| {
+        println!("Could not read relocations: {:?}", e);
+        Default::default()
+    });
     let base = 0x400000_usize;
 
-    println!("Mapping {:?} in memory...", input_path);
+    println!("Loading with base address @ 0x{:x}", base);
 
-    let mut mappings = Vec::new();
-
-    for ph in file
+    let non_empty_load_segments = file
         .program_headers
         .iter()
         .filter(|ph| ph.r#type == delf::SegmentType::Load)
-        .filter(|ph| ph.mem_range().end > ph.mem_range().start)
-    {
-        println!("Mapping segment @ {:?} with {:?}", ph.mem_range(), ph.flags);
+        .filter(|ph| ph.mem_range().end > ph.mem_range().start);
 
+    let mut mappings = Vec::new();
+    for ph in non_empty_load_segments {
+        println!("Mapping {:?} - {:?}", ph.mem_range(), ph.flags);
         let mem_range = ph.mem_range();
         let len: usize = (mem_range.end - mem_range.start).into();
+
         let start: usize = mem_range.start.0 as usize + base;
         let aligned_start: usize = align_lo(start);
-
         let padding = start - aligned_start;
         let len = len + padding;
 
-        let addr: *mut u8 = aligned_start as _;
+        let addr: *mut u8 = unsafe { std::mem::transmute(aligned_start) };
 
-        println!("Addr: {:p}, Padding: {:08x}", addr, padding);
+        if padding > 0 {
+            println!("(With 0x{:x} bytes of padding at the start)", padding);
+        }
 
         let map = MemoryMap::new(len, &[MapOption::MapWritable, MapOption::MapAddr(addr)])?;
 
-        println!("Copying segment data...");
-        {
-            let dst = unsafe { std::slice::from_raw_parts_mut(addr.add(padding), ph.data.len()) };
-
-            dst.copy_from_slice(&ph.data[..]);
+        unsafe {
+            copy_nonoverlapping(ph.data.as_ptr(), addr.add(padding), len);
         }
 
-        println!("Adjusting permissions...");
+        let mut num_relocs = 0;
+        for reloc in &rela_entries {
+            if mem_range.contains(&reloc.offset) {
+                num_relocs += 1;
+                unsafe {
+                    let real_segment_start = addr.add(padding);
+                    let offset_into_segment = reloc.offset - mem_range.start;
+                    let reloc_addr = real_segment_start.add(offset_into_segment.into());
+                    match reloc.r#type {
+                        delf::RelType::Relative => {
+                            let reloc_addr: *mut u64 = transmute(reloc_addr);
+                            let reloc_value = reloc.addend + delf::Addr(base as u64);
+                            std::ptr::write_unaligned(reloc_addr, reloc_value.0);
+                        }
+                        typ => {
+                            panic!("Unsupported relocation type {:?}", typ);
+                        }
+                    }
+                }
+            }
+        }
+
+        if num_relocs > 0 {
+            println!("(Applied {} relocations)", num_relocs);
+        }
+
         let mut protection = Protection::NONE;
         for flag in ph.flags.iter() {
             protection |= match flag {
@@ -77,7 +95,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     println!("Jumping to entry point @ {:?}...", file.entry_point);
-    pause("jmp")?;
 
     unsafe {
         jmp((file.entry_point.0 as usize + base) as _);
@@ -85,35 +102,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn ndisasm(code: &[u8], origin: delf::Addr) -> Result<(), Box<dyn Error>> {
-    let mut child = Command::new("ndisasm")
-        .arg("-b")
-        .arg("64")
-        .arg("-o")
-        .arg(format!("{}", origin.0))
-        .arg("-")
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()?;
-
-    child.stdin.as_mut().unwrap().write_all(code)?;
-    let output = child.wait_with_output()?;
-    println!("{}", String::from_utf8_lossy(&output.stdout));
-    Ok(())
-}
-
 unsafe fn jmp(addr: *const u8) {
     let fn_ptr: fn() = std::mem::transmute(addr);
     fn_ptr();
-}
-
-fn pause(reason: &str) -> Result<(), Box<dyn Error>> {
-    println!("Press Enter to {}...", reason);
-    {
-        let mut s = String::new();
-        std::io::stdin().read_line(&mut s)?;
-    }
-    Ok(())
 }
 
 fn align_lo(x: usize) -> usize {
